26bacec6d442ddc3c5643d7c67b5552ac5dfdd30
AndyD-ev <daol.99110@gmail.com>
2026-05-18 23:36:58 -0600

    Changes 2 in app.js

--- src/app.js
+++ src/app.js
@@ -3,13 +3,15 @@ const cors = require("cors")
 
 const authRoutes = require("./routes/authRoutes")
 const employeeRoutes = require("./routes/employeeRoutes")
+
 const app = express()
 
-//app.use(cors())
-
 app.use(cors({
-  origin: "https://administrative-system.vercel.app"
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }))
+
 app.use(express.json())
 
 app.use("/auth", authRoutes)
