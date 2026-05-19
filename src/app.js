fb0e9f0c5a5337454c763c3b3fee8a724da96af6
AndyD-ev <daol.99110@gmail.com>
2026-05-19 00:27:25 -0600

    Changes 6 in app.js

--- src/app.js
+++ src/app.js
@@ -6,12 +6,9 @@ const employeeRoutes = require("./routes/employeeRoutes")
 
 const app = express()
 
-app.use(cors({
-  origin: [
-    "https://administrative-system.vercel.app",
-    "https://administrative-system-a4g36e0eq-andyd-evs-projects.vercel.app"
-  ]
-}))
+app.use(cors())
+
+app.options("*", cors())
 
 app.use(express.json())
 
